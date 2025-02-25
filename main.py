ff085fb616d0f8a1c3b3c4f03a3b511a14567858
Zhoe Aeris <zhoe_gongon@dlsu.edu.ph>
2025-02-25 21:39:38 +0800

    updated main.py for updated datasets

--- main.py
+++ main.py
@@ -1,46 +1,83 @@
 from config import train_config
-from train import distributed_train,main_worker
+from train import distributed_train
 from evaluation import all_eval
 import argparse
 import fire
 import torch
 import subprocess
-#torch.autograd.set_detect_anomaly(True)
 
+# torch.autograd.set_detect_anomaly(True)
+
+# Pretrain the model using frame-based datasets (FF++ C23, C40, CelebDF, WildDeepfake)
 def pretrain():
-    name='Efb4'
-    url='tcp://127.0.0.1:27015'
-    Config=train_config(name,['ff-all-c23','efficientnet-b4'],url=url,attention_layer='b5',feature_layer='logits',epochs=20,batch_size=16,AGDA_loss_weight=0)
+    name = 'Efb4'
+    url = 'tcp://127.0.0.1:27015'
+
+    # 🔹 Ensure dataset uses pre-extracted frames instead of videos
+    Config = train_config(
+        name,
+        ['ff-c23-c40-celebdf-wilddeepfake', 'efficientnet-b4'], 
+        url=url,
+        attention_layer='b5',
+        feature_layer='b1',
+        epochs=20,
+        batch_size=64,  # Increased batch size for frame training
+        AGDA_loss_weight=0
+    )
+
     Config.mkdirs()
-    distributed_train(Config) 
-    procs=[subprocess.Popen(['/bin/bash','-c','CUDA_VISIBLE_DEVICES={} python main.py test {} {}'.format(i,name,j)]) for i,j in enumerate(range(-3,0))]
+    distributed_train(Config)
+
+    # Run evaluation on the last 3 checkpoints
+    procs = [subprocess.Popen(['/bin/bash', '-c', f'CUDA_VISIBLE_DEVICES={i} python main.py test {name} {j}'])
+             for i, j in enumerate(range(-3, 0))]
+    
     for i in procs:
         i.wait()
 
-## do pretrain first!
-def aexp():
-    name='a1_b5_b2'  
-    url='tcp://127.0.0.1:27016'
-    Config=train_config(name,['ff-all-c23','efficientnet-b4'],url=url,attention_layer='b5',feature_layer='b2',epochs=50,batch_size=15,\
-        ckpt='checkpoints/Efb4/ckpt_19.pth',inner_margin=[0.2,-0.8],margin=0.8)
+# Run a custom experiment with modified parameters
+def experiment():
+    name = 'exp1_b5_b2'
+    url = 'tcp://127.0.0.1:27016'
+
+    # 🔹 Adjusted dataset structure for pre-extracted frames
+    Config = train_config(
+        name,
+        ['ff-c23-c40-celebdf-wilddeepfake', 'efficientnet-b4'],
+        url=url,
+        attention_layer='b5',
+        feature_layer='b2',
+        epochs=50,
+        batch_size=32,  # Reduce batch size if memory is an issue
+        ckpt='checkpoints/Efb4/ckpt_19.pth',
+        inner_margin=[0.2, -0.8],
+        margin=0.8
+    )
+
     Config.mkdirs()
-    distributed_train(Config) 
-    procs=[subprocess.Popen(['/bin/bash','-c','CUDA_VISIBLE_DEVICES={} python main.py test {} {}'.format(i,name,j)]) for i,j in enumerate(range(-3,0))]
+    distributed_train(Config)
+
+    procs = [subprocess.Popen(['/bin/bash', '-c', f'CUDA_VISIBLE_DEVICES={i} python main.py test {name} {j}'])
+             for i, j in enumerate(range(-3, 0))]
+
     for i in procs:
         i.wait()
 
-
-def resume(name,epochs=0):
-    Config=train_config.load(name)
-    Config.epochs+=epochs
+# Resume training from the last checkpoint
+def resume(name, epochs=0):
+    Config = train_config.load(name)
+    Config.epochs += epochs
     Config.reload()
-    Config.resume_optim=True
-    distributed_train(Config) 
-    for i in range(-3,0):
-        all_eval(name,i)
+    Config.resume_optim = True
 
-def test(name,ckpt=None):
-    all_eval(name,ckpt)
-        
-if __name__=="__main__":
+    distributed_train(Config)
+
+    for i in range(-3, 0):
+        all_eval(name, i)
+
+# Run evaluation on a specific checkpoint.
+def test(name, ckpt=None):
+    all_eval(name, ckpt)
+
+if __name__ == "__main__":
     fire.Fire()
