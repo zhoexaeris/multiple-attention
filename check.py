952f176a498f5b768384709fad34b2d0789c34d9
Zhoe Aeris <zhoe_gongon@dlsu.edu.ph>
2025-02-25 21:10:22 +0800

    updated dataset.py to load frames directly

---
+++ check.py
@@ -0,0 +1,2 @@
+import torch
+print(torch.cuda.device_count())  # Should return the number of available GPUs
--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,88 +1,77 @@
 import os
 import torch
-from PIL import Image
-from torch.utils.data import Dataset
-from datasets.augmentations import augmentations
-from albumentations import CenterCrop,Compose,Resize,RandomCrop
-from albumentations.pytorch.transforms import ToTensor
-import json
-import random
 import cv2
-from datasets.data import *
+from torch.utils.data import Dataset
+from albumentations import CenterCrop, Compose
+from albumentations.pytorch.transforms import ToTensor
+from datasets.data import *  # Import dataset utilities
 
 class DeepfakeDataset(Dataset):
-
-    def __init__(self,phase='train',datalabel='', resize=(320,320),imgs_per_video=30,min_frames=0,\
-    normalize=dict(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5]),frame_interval=10,max_frames=300,augment='augment0'):
+    def __init__(self, phase='train', datalabel='', resize=(320, 320),
+                 normalize=dict(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])):
         assert phase in ['train', 'val', 'test']
-        self.datalabel=datalabel
         self.phase = phase
-        self.imgs_per_video=imgs_per_video
-        self.frame_interval=frame_interval
-        self.num_classes = 2
-        self.epoch=0
-        self.max_frames=max_frames
-        if min_frames:
-            self.min_frames=min_frames
-        else:
-            self.min_frames=max_frames*0.3
-        self.dataset=[]
-        self.aug=augmentations[augment]
-        resize_=(int(resize[0]/0.8),int(resize[1]/0.8))
-        self.resize=resize
-        #Resize(*resize_,interpolation=cv2.INTER_CUBIC),
-        self.trans=Compose([CenterCrop(*resize),ToTensor(normalize=normalize)])
-        ###############
-        # doing resize and center crop in trans
-        if type(datalabel)!=str:
-            self.dataset=datalabel
-            return
-        if 'ff-5' in self.datalabel:
-            for i,j in enumerate(['Origin','Deepfakes','NeuralTextures','FaceSwap','Face2Face']):
-                temp=FF_dataset(j,self.datalabel.split('-')[2],phase)
-                temp=[[k[0],i]  for k in temp ]
-                self.dataset+=temp
-        elif 'ff-all' in self.datalabel:
-            for i in ['Origin','Deepfakes','NeuralTextures','FaceSwap','Face2Face']:
-                self.dataset+=FF_dataset(i,self.datalabel.split('-')[2],phase)
-            if phase!='test':
-                self.dataset=make_balance(self.dataset)
-        elif 'ff' in self.datalabel:
-            self.dataset+=FF_dataset(self.datalabel.split('-')[1],self.datalabel.split('-')[2],phase)+FF_dataset("Origin",self.datalabel.split('-')[2],phase)
-        elif 'celeb' in self.datalabel:
-            self.dataset=Celeb_test
-        elif 'deeper' in self.datalabel:
-            self.dataset=deeperforensics_dataset(phase)+FF_dataset('Origin',self.datalabel.split('-')[1],phase)
-        elif 'dfdc' in self.datalabel:
-            self.dataset=dfdc_dataset(phase)
-        else: raise(Exception('no such datset'))
+        self.datalabel = datalabel
+        self.resize = resize
+        self.normalize = normalize
+        self.epoch = 0  # Keep epoch logic to shuffle frames if needed
+
+        # Define transformations (Only Resize & Normalize)
+        self.trans = Compose([
+            CenterCrop(*resize),  # Apply Center Crop
+            ToTensor(normalize=normalize)  # Normalize pixel values
+        ])
+
+        # Load dataset file paths
+        self.dataset = self.load_dataset()
+
+    def load_dataset(self):
+        """Loads frame paths and labels for each dataset."""
+        dataset = []
+        
+        # Define dataset root paths
+        dataset_roots = {
+            'ff-c23': 'datasets/ffpp/c23/',
+            'ff-c40': 'datasets/ffpp/c40/',
+            'celebdf': 'datasets/celebDF/',
+            'wild': 'datasets/wilddeepfake/'
+        }
+
+        # Iterate through dataset types
+        for dataset_name, root in dataset_roots.items():
+            if dataset_name in self.datalabel:
+                for class_label, class_folder in enumerate(['real', 'fake']):  # 0 for real, 1 for fake
+                    class_path = os.path.join(root, class_folder)
+                    if os.path.exists(class_path):
+                        frame_files = [os.path.join(class_path, f) for f in os.listdir(class_path) if f.endswith(('.png', '.jpg'))]
+                        dataset.extend([[fp, class_label] for fp in frame_files])
+
+        if not dataset:
+            raise Exception("No frames found! Check dataset paths.")
+
+        return dataset
 
     def next_epoch(self):
-        self.epoch+=1
+        """ Shuffles dataset for the next epoch (optional) """
+        self.epoch += 1
+        random.shuffle(self.dataset)
 
-    def __getitem__(self, item):
+    def __getitem__(self, index):
+        """Load and process image frame."""
         try:
-            vid=self.dataset[item//self.imgs_per_video]
-            vd=sorted(os.listdir(vid[0]))
-            if len(vd)<self.min_frames:
-                raise(Exception(str(vid)))
-                #return self.__getitem__((item+self.imgs_per_video)%(self.__len__()))
-            ind=(item%self.imgs_per_video*self.frame_interval+self.epoch)%min(len(vd),self.max_frames)
-            ind=vd[ind]
-            image =cv2.imread(os.path.join(vid[0],ind))
-            image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            if self.phase=='train':
-                image=self.aug(image=image)['image']
-            return self.trans(image=image)['image'], vid[1]
+            frame_path, label = self.dataset[index]
+            image = cv2.imread(frame_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
+
+            # Apply transformations (Resizing & Normalization)
+            image = self.trans(image=image)['image']
+
+            return image, label
+
         except Exception as e:
-            #print(e)
-            #raise(e)
-            if self.phase!='test':
-                return self.__getitem__((item+self.imgs_per_video)%(self.__len__()))
-            else:
-                return torch.zeros(3,self.resize[0],self.resize[1]),-1
+            print(f"Error loading frame: {e}")
+            return torch.zeros(3, self.resize[0], self.resize[1]), -1  # Return blank tensor in case of failure
 
     def __len__(self):
-        return len(self.dataset)*self.imgs_per_video
-
-
+        """Returns dataset size."""
+        return len(self.dataset)
